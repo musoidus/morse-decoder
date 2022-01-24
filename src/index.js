93297a0084553e16782f6d91fdafa781823d46d0
Nina <nina.korocevic@icloud.com>
2022-01-24 17:30:47 +0300

    refactor: rename vars and rearrange code for better readability

--- src/index.js
+++ src/index.js
@@ -39,22 +39,26 @@ const MORSE_TABLE = {
 
 function decode(expr) {
   const codedLettersArr = [];
-  let codedLetterSplitted = [];
+  let morseSymbol = '';
+  let morseCode = [];
   let output = '';
+  let codedLetterPart = '';
+
   for (let i = 0; i <= expr.length - 1; i += 10) {
     codedLettersArr.push(expr.slice(i, i + 10));
   }
+
   for (let codedLetter of codedLettersArr) {
     if (!codedLetter.startsWith('*')) {
       for (let i = 0; i <= codedLetter.length - 1; i += 2) {
-        let morseSign = '';
-        if (codedLetter.slice(i, i + 2) !== '00') {
-          morseSign = codedLetter.slice(i, i + 2) === '10' ? '.' : '-';
+        codedLetterPart = codedLetter.slice(i, i + 2);
+        if (codedLetterPart !== '00') {
+          morseSymbol = codedLetterPart === '10' ? '.' : '-';
+          morseCode.push(morseSymbol);
         }
-        codedLetterSplitted.push(morseSign);
       }
-      output += MORSE_TABLE[codedLetterSplitted.join('')];
-      codedLetterSplitted.length = 0;
+      output += MORSE_TABLE[morseCode.join('')];
+      morseCode.length = 0;
     } else output += ' ';
   }
   return output;
